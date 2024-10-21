5f97430eeb2c05854ab75e9133d61daf058e7d13
Leonardo <leomilho@gmail.com>
2024-10-21 13:26:45 +0200

    sync_to_async support

--- casbin_adapter/apps.py
+++ casbin_adapter/apps.py
@@ -1,13 +1,18 @@
 from django.apps import AppConfig
 from django.conf import settings
 from asgiref import sync_to_async
+import asyncio
+
+
+async def setup():
+    from .enforcer import initialize_enforcer
+
+    db_alias = getattr(settings, "CASBIN_DB_ALIAS", "default")
+    await sync_to_async(initialize_enforcer)(db_alias)
 
 
 class CasbinAdapterConfig(AppConfig):
     name = "casbin_adapter"
 
     def ready(self):
-        from .enforcer import initialize_enforcer
-
-        db_alias = getattr(settings, "CASBIN_DB_ALIAS", "default")
-        sync_to_async(initialize_enforcer)(db_alias)
+        asyncio.create_task(setup())
